14d81ac5ddce919387ec66b31ffeca73721edfa3
Wouter <nomail@hotmail.com>
2026-05-13 11:14:47 +0200

    merge tests

--- src/test/java/net/sf/jannot/TestFeature.java
+++ src/test/java/net/sf/jannot/TestFeature.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 import org.junit.Assert;
@@ -16,6 +17,21 @@ import org.junit.Test;
  * 
  */
 public class TestFeature {
+
+	private final static Location loc1 = new Location(10, 20);
+
+	@Test
+	public void smoke() {
+		new Feature(loc1, null, Strand.UNKNOWN);
+	}
+
+	@Test
+	public void testConstructorsEqual() {
+		Feature f1 = new Feature(loc1, null, Strand.UNKNOWN);
+		Feature f2 = new Feature(new HashSet<>(Arrays.asList(loc1)), null,
+				Strand.UNKNOWN);
+		assertEquals(f1, f2);
+	}
 
 //	@Ignore // test not working
 //	@Test
