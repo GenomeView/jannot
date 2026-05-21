3867e03f5d720b4dee6987599974e5f513ff0024
Wouter <nomail@hotmail.com>
2026-05-21 17:24:53 +0200

    javadoc

--- src/main/java/net/sf/jannot/source/cache/SourceCache.java
+++ src/main/java/net/sf/jannot/source/cache/SourceCache.java
@@ -18,11 +18,21 @@ import net.sf.jannot.Global;
 import net.sf.jannot.source.DataSource;
 import net.sf.jannot.source.FileSource;
 
+/**
+ * Utility class. Methods to store streams to be cached in user home directory ,
+ * After calling {@link #startCaching(URL)} a file N.tmp is created where N is
+ * the MD5 hash code of the URL. Then after calling {@link #finish(URL)} the
+ * file is closed and renamed to N.url. To get a cached version of the URL, use
+ * {@link #get(URL, Global)}
+ */
 public class SourceCache {
-	// singleton class with only static members.
-
 	public static File cacheDir = new File(System.getProperty("user.home"));;
 
+	/**
+	 * 
+	 * @param url
+	 * @return true iff the URL has been cached.
+	 */
 	public static boolean contains(URL url) {
 
 		// System.out.println("URL cache: " + cacheDir);
@@ -40,6 +50,13 @@ public class SourceCache {
 
 	}
 
+	/**
+	 * 
+	 * @param url    the data needed
+	 * @param global the {@link Global} vars
+	 * @return {@link DataSource} constructed from the cached version of the URL
+	 * @throws IOException
+	 */
 	public static DataSource get(URL url, Global global) throws IOException {
 		// System.out.println("Retrieving from cache: " + url);
 		return new FileSource(
@@ -47,6 +64,14 @@ public class SourceCache {
 				global);
 	}
 
+	/**
+	 * 
+	 * @param url an {@link URL} that is intended for caching
+	 * @return an OutputStream to the cache file for the URL. The called becomes
+	 *         the owner of the stream and should close it and then call
+	 *         {@link #finish(URL)} when done.
+	 * @throws FileNotFoundException
+	 */
 	public static OutputStream startCaching(URL url)
 			throws FileNotFoundException {
 		return new FileOutputStream(
@@ -54,9 +79,9 @@ public class SourceCache {
 	}
 
 	/**
-	 * This method should only be called when the data is completely written.
+	 * Rename the cached file to .url. Assumes the data is completely written.
 	 * 
-	 * @param url
+	 * @param url the {@link URL} for which the cache file is complete
 	 */
 	public static void finish(URL url) {
 		File f = new File(cacheDir, MD5Tools.md5(url.toString()) + ".tmp");
